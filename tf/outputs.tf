3fc4fb8053b07cfb7ab123641737627869e17188
Mathieu Benoit <mabenoit@microsoft.com>
2019-12-31 17:12:15 -0500

    [skip ci] fix interpolation warning

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -1,4 +1,4 @@
 output "resource_group_id" {
   description = "Example of output to get the resource group id"
-  value       = "${azurerm_resource_group.rg.id}"
+  value       = azurerm_resource_group.rg.id
 }
