1cd9248b759f37f0f468a8991d203b12f34683b2
Mathieu Benoit <mabenoit@microsoft.com>
2019-12-31 16:57:59 -0500

    azurerm:1.39.0 + fix “Interpolation-only expressions are deprecated” warning

--- tf/main.tf
+++ tf/main.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
-  version = "=1.38.0"
+  version = "=1.39.0"
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.resource_group_name}"
-  location = "${var.location}"
+  name     = var.resource_group_name
+  location = var.location
 }
